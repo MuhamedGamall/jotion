ca2be618f697b6051bb9d22174b054bc454393b5
JimmyDeveloperMan <jimmy.moftah@gmail.com>
2024-08-18 18:46:42 +0300

    setup project

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://verified-doberman-95.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
